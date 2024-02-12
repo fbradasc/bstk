d735e9e021ce4acacfe9044aaa0da52146df6a42
fbradasc@gmail.com <fbradasc@gmail.com>
2024-02-12 18:35:44 +0100

    Adding openldap

--- bstk.sh
+++ bstk.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/install/lib:${PWD}/install/lib64
+export LD_LIBRARY_PATH=${PWD}/install/lib:${PWD}/install/lib64
 
 export PATH=${PWD}/install/bin:${PATH}
+
+export LD_PRELOAD=${PWD}/install/lib64/libssl.so.3
 
 baresip ${*}
